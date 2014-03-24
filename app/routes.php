2155041dbd19d494d59d53e0b8f435a6774bb2cc
Corey Kepple <coreyckepple@gmail.com>
2014-03-24 11:32:57 -0500

    initial commit.

---
+++ app/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('/sayhello', function()
+{
+    return "Hello, Codeup!";
+});
+
+Route::get('/resume', function()
+{
+    return "This is my Resume.";
+});
+
+Route::get('/portfolio', function()
+{
+    return "This is my Portfolio.";
+});
+
+Route::get('/sayhello/{name}', function($name)
+{
+    if ($name == "corey")
+    {
+        return Redirect::to('/');
+    }
+    else
+    {
+        return "Hello, $name!";
+    }
+});
