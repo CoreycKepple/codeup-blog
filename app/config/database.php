b8b9692a886c71a0c575b60c7b60e16ee3ac33e1
Corey Kepple <coreyckepple@gmail.com>
2014-03-28 20:47:13 -0500

    changed host setting of database config

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 		    'driver'    => 'mysql',
-		    'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+		    'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : '127.0.0.1',
 		    'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'blog_db',
 		    'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'corey',
 		    'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'password',
