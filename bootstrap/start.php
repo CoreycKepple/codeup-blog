5c77cf6fcb5f9ce909c83234c8b6cbcb9f3f376f
Corey Kepple <coreyckepple@gmail.com>
2014-03-31 09:12:06 -0500

    changed machine name.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('corey kepple’s MacBook'),
 	'production' => array('tunnel.pagodabox.com')
 
 ));
